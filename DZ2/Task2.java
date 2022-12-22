14ecd3a423f265ad9b1493c54b63bb927c0a25cf
Denis <9581886@gmail.com>
2022-12-22 22:17:11 +0300

    Файлы ДЗ2 переделал task2 и task3

--- DZ2/Task2.java
+++ DZ2/Task2.java
@@ -11,12 +11,14 @@ public class Task2 {
             
         try {
            int d = 0;
-           int intArray=8;
-           double catchedRes1 = intArray / d; // оставил, чтобы посмотреть как поймается ошибка
+           int[] intArray = new int [7];
+           double catchedRes1 = intArray[8] / d; // оставил, чтобы посмотреть как поймается ошибка
            System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
-        }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index exception: " + e);
+        } 
     }
 
 
--- DZ2/Task3.java
+++ DZ2/Task3.java
@@ -1,44 +1,88 @@
+// import java.util.Scanner;
+
+// public class Task3 {
+//     // Задание 3
+
+//     public static void main(String[] args) throws Exception {
+//         try {
+//             int a = 90;
+//             int b = 3;
+//             System.out.println(a / b);
+//             printSum(23, 234);
+//             newString();
+//             int[] abc = { 1, 2 };
+//             abc[3] = 9;
+            
+//         } catch (IllegalArgumentException ex) {
+//             System.out.println("Строка не должна быть пустой!");
+//         } catch (NullPointerException ex) {
+//             System.out.println("Указатель не может указывать на null!");
+//         } catch (IndexOutOfBoundsException ex) {
+//             System.out.println("Массив выходит за пределы своего размера!");
+//         } catch (Throwable ex) {
+//             System.out.println("Что-то пошло не так...");
+//         }
+//     }
+
+//     private static void newString() throws Exception {
+//         Scanner str = new Scanner(System.in);
+//         System.out.println("Введите строку: ");
+//         String newLine = str.nextLine();
+//         if (newLine.equals("")) {
+//             str.close();
+//             throw new IllegalArgumentException("Строка не должна быть пустой!");
+//         }
+//         str.close();
+//     }
+
+//     public static void printSum(Integer a, Integer b) throws Exception {
+//          System.out.println(a + b);
+// }
+// }
+
 import java.util.Scanner;
 
 public class Task3 {
     // Задание 3
 
     public static void main(String[] args) throws Exception {
-        try {
+        // try {
             int a = 90;
             int b = 3;
             System.out.println(a / b);
             printSum(23, 234);
             newString();
             int[] abc = { 1, 2 };
-            abc[3] = 9;
+            int index=3;
+            if (index<abc.length) {
+                abc[index] = 9;
+            } else {
+                System.out.println("Индекс ["+ index + "] за пределами массива! Длина массива: " + abc.length);
+            }
             
-        } catch (IllegalArgumentException ex) {
-            System.out.println("Строка не должна быть пустой!");
-        } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!");
-        } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Массив выходит за пределы своего размера!");
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        }
+        // } catch (IllegalArgumentException ex) {
+        //     System.out.println("Строка не должна быть пустой!");
+        // } catch (NullPointerException ex) {
+        //     System.out.println("Указатель не может указывать на null!");
+        // } catch (IndexOutOfBoundsException ex) {
+        //     System.out.println("Массив выходит за пределы своего размера!");
+        // } catch (RuntimeException ex) {
+        //     System.out.println("Что-то пошло не так..." + ex);
+        // }
     }
 
-    private static void newString() throws Exception {
-        Scanner str = new Scanner(System.in);
+    private static void newString() {
+        try(Scanner str = new Scanner(System.in)) {
         System.out.println("Введите строку: ");
         String newLine = str.nextLine();
-        if (newLine.equals("")) {
-            str.close();
-            throw new IllegalArgumentException("Строка не должна быть пустой!");
+        while (newLine.equals("")) {
+            System.out.println("Строка не должна быть пустой!");
+            newLine = str.nextLine();
         }
-        str.close();
+        }
     }
 
-    public static void printSum(Integer a, Integer b) throws Exception {
+    public static void printSum(Integer a, Integer b) {
          System.out.println(a + b);
 }
-
-
-
 }
